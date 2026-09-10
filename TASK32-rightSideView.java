fdec2c12ca6cc6381b0d1b844709de186f807853
lsvphanidhar31 <lsvphanidhar31@gmail.com>
2026-09-10 08:58:13 +0530

    Create TASK32-rightSideView.java

---
+++ TASK32-rightSideView.java
@@ -0,0 +1,30 @@
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+
+        if (root == null)
+            return ans;
+
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+
+        while (!q.isEmpty()) {
+            int n = q.size();
+
+            for (int i = 0; i < n; i++) {
+                TreeNode node = q.poll();
+
+                if (i == n - 1)
+                    ans.add(node.val);
+
+                if (node.left != null)
+                    q.add(node.left);
+
+                if (node.right != null)
+                    q.add(node.right);
+            }
+        }
+
+        return ans;
+    }
+}
