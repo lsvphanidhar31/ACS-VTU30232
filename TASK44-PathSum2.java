9c9c80a197f34f7dfc15b07e7f8314239fee46d1
lsvphanidhar31 <lsvphanidhar31@gmail.com>
2026-09-24 09:02:28 +0530

    Create TASK44-PathSum2.java

---
+++ TASK44-PathSum2.java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> path = new ArrayList<>();
+
+        dfs(root, targetSum, path, result);
+
+        return result;
+    }
+
+    void dfs(TreeNode root, int target, List<Integer> path,
+             List<List<Integer>> result) {
+
+        if (root == null)
+            return;
+
+        path.add(root.val);
+
+        if (root.left == null && root.right == null &&
+            target == root.val) {
+            result.add(new ArrayList<>(path));
+        }
+
+        dfs(root.left, target - root.val, path, result);
+        dfs(root.right, target - root.val, path, result);
+
+        path.remove(path.size() - 1);
+    }
+}
