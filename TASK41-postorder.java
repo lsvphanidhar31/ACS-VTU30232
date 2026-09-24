2e19824e08eb4167f9b1822f49998bcb33c04d89
lsvphanidhar31 <lsvphanidhar31@gmail.com>
2026-09-24 08:58:42 +0530

    Create TASK41-postorder.java

---
+++ TASK41-postorder.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+
+        postorder(root, result);
+
+        return result;
+    }
+
+    void postorder(TreeNode root, List<Integer> result) {
+        if (root == null)
+            return;
+
+        postorder(root.left, result);   
+        postorder(root.right, result);  
+        result.add(root.val);           
+    }
+}
